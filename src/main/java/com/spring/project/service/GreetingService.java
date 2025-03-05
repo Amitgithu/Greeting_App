a672e30685365c3cf0c9d1232ae49f8527c0ca94
Amit Kumar <amit909563@gmail.com>
2025-03-05 14:10:10 +0530

    Added: Find Greeting Message By ID

---
+++ src/main/java/com/spring/project/service/GreetingService.java
@@ -0,0 +1,30 @@
+package com.spring.project.service;
+
+import com.spring.project.dto.GreetingDTO;
+import com.spring.project.dto.UserDTO;
+import com.spring.project.repository.GreetingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@Service
+public class GreetingService implements IGreetingService {
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @Autowired
+    private GreetingRepository greetingRepository;
+
+    @Override
+    public GreetingDTO addGreeting(UserDTO user) {
+        String message = String.format(template, (user.getFirstName().isEmpty() && user.getLastName().isEmpty()) ? "World" : user.getFirstName() + " " + user.getLastName());
+        return greetingRepository.save(new GreetingDTO(counter.incrementAndGet(), message));
+    }
+
+    @Override
+    public GreetingDTO getGreetingById(long id) {
+        return greetingRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Greeting not found with id: " + id));
+    }
+}
---
+++ src/main/java/com/spring/project/service/IGreetingService.java
@@ -0,0 +1,10 @@
+package com.spring.project.service;
+
+import com.spring.project.dto.GreetingDTO;
+import com.spring.project.dto.UserDTO;
+
+
+public interface IGreetingService {
+    GreetingDTO addGreeting(UserDTO user);
+    GreetingDTO getGreetingById(long id);
+}

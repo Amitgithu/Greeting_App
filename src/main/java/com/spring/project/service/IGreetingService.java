ec0069364e334e8599567114166943df9dbb2e3c
Amit Kumar <amit909563@gmail.com>
2025-03-05 14:15:00 +0530

    Added: Update the Existed Greeting

---
+++ src/main/java/com/spring/project/service/IGreetingService.java
@@ -0,0 +1,13 @@
+package com.spring.project.service;
+
+import com.spring.project.dto.GreetingDTO;
+import com.spring.project.dto.UserDTO;
+import java.util.List;
+
+
+public interface IGreetingService {
+    GreetingDTO addGreeting(UserDTO user);
+    GreetingDTO getGreetingById(long id);
+    List<GreetingDTO> getAllGreetings();
+    GreetingDTO editGreeting(long id, UserDTO user);
+}
